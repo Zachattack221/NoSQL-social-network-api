bc5d6cd8b3da6d374af9d0fc225602d69b44b56e
Zachattack221 <zsvd09@gmail.com>
2022-10-12 18:13:48 -0400

    migrated my reaction schema to be used within thought model, updated the rest of thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,41 +1,57 @@
-const { Schema, model } = require('mongoose');
-const { stringify } = require('querystring');
-const Reaction = require('./Reaction');
+const { Schema, Types, model } = require('mongoose');
+// TODO: add logic for pulling date 
 
-// // TODO: 
-// Thought class
 
-// thoughtText needs: String, Required, Must be between 1 and 280 characters
-
-// createdAt needs: Date, Set default value to the current timestamp, Use a getter method to format the timestamp on query
-
-// username of user who created the thought: string, required
-
-// reactions needs: array of nested documents with reactionSchema
-
-// create virtual reactionCount to pull length of the thought's reactions array on query
-
-// Schema to create Post model
-const thoughtSchema = new Schema(
+// Schema to create reaction virtual
+const ReactionSchema = new Schema(
   {
-    published: {
-      type: Boolean,
-      default: false,
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
       default: Date.now,
+      // get:TODO: ()
     },
-    advertiserFriendly: {
-      type: Boolean,
-      default: true,
+  },
+  {
+    toJSON: {
+      getters: true,
     },
-    description: {
+    id: false,
+  }
+);
+
+// Schema to create thought model
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
       type: String,
-      minLength: 15,
-      maxLength: 500,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
     },
-    responses: [Reaction],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // get: TODO:()
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema]
   },
   {
     toJSON: {
@@ -45,17 +61,10 @@ const thoughtSchema = new Schema(
   }
 );
 
-const Thought = model('thought.username', thoughtSchema);
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
 
 module.exports = Thought;
-
-
-// Create a virtual property `responses` that gets the amount of response per video
-// videoSchema
-//   .virtual('getResponses')
-//   // Getter
-//   .get(function () {
-//     return this.responses.length;
-//   });
-
-// Initialize our Thought model, may need editing 
