08eb8d1fed6849485f0c342cf9d58c3f2fa48295
Zachattack221 <zsvd09@gmail.com>
2022-10-13 18:35:19 -0400

    working on setup of utils folder, getting seed.js to properly clear/seed data from data.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost:27017/nosql-social-network-api', {
+const connectionString =   process.env.MONGODB_URI || 'mongodb://localhost:27017/nosql-social-network-api';
+
+connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
--- server.js
+++ server.js
@@ -1,45 +1,38 @@
 const express = require('express');
-const mongodb = require('mongodb').MongoClient;
-// const db = require('./config/connection');
+// const mongodb = require('mongodb').MongoClient;
+const db = require('./config/connection');
 const routes = require('./routes');
 
-// const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-let db;
-
-
-
-const connectionStringURI = 'mongodb://localhost:27017/nosql-social-network-api';
+// let db;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(routes);
 
-// db.once('open', () => {
-//   app.listen(PORT, () => {
-//     console.log(`API server for ${activity} running on port ${PORT}!`);
-//   });
-// });
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
 
-mongodb.connect(
-  // Defines connection between app and MongoDB instance
-  connectionStringURI,
-  // Sets connection string parser and Server Discover and Monitoring engine to true and avoids warning
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    // Use client.db() constructor to add new db instance
-    db = client.db();
-    app.listen(PORT, () => {
-      console.log(`Example app listening at http://localhost:${PORT}`);
-    });
-  }
-);
+// mongodb.connect(
+//   // Defines connection between app and MongoDB instance
+//   connectionStringURI,
+//   // Sets connection string parser and Server Discover and Monitoring engine to true and avoids warning
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   (err, client) => {
+//     // Use client.db() constructor to add new db instance
+//     db = client.db();
+//     app.listen(PORT, () => {
+//       console.log(`Example app listening at http://localhost:${PORT}`);
+//     });
+//   }
+// );
 
-
-// TODO: update project routes, isolate to their own connection pages
 
 
